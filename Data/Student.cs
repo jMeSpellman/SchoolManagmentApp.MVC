5905323ae86f427c1d3e4f0133bee0e79f5169f4
jMeSpellman <jatrill@spellmanhv.co.uk>
2024-11-29 13:26:40 +0000

    Finished customizing Views and Elements

--- Data/Student.cs
+++ Data/Student.cs
@@ -12,4 +12,6 @@ public partial class Student
     public string LastName { get; set; } = null!;
 
     public DateOnly? DataOfBirth { get; set; }
+
+    public virtual ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
 }
