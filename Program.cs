f8d5db2d36526780917bc99e2036c7fb0323748c
jMeSpellman <jatrill@spellmanhv.co.uk>
2024-12-04 13:14:17 +0000

    Added Auth0 but could not validate account

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Auth0.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
 using SchoolManagmentApp.MVC.Data;
 
@@ -7,6 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 var conn = builder.Configuration.GetConnectionString("SchoolManagementDbConnection");
 builder.Services.AddDbContext<SchoolManagementDbContext>(q => q.UseSqlServer(conn));
+
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = builder.Configuration["Auth0:Domain"];
+    options.ClientId = builder.Configuration["Auth0:ClientId"];
+});
 
 builder.Services.AddControllersWithViews();
 
@@ -25,6 +32,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
